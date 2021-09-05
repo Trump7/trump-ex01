a17208820192efd87f68c7a2e173fe91fb2d9cf0
Trump7 <trump@knights.ucf.edu>
2021-09-05 14:30:31 -0400

    First commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+
+        String n = input.next();
+
+        System.out.println("Hello, " + n + ", nice to meet you!");
+    }
+}
